4574cc060cee72e7fa639f4d8525449fd08217f2
Othman Ali <64964203+OsmanAliMohamed@users.noreply.github.com>
2022-05-26 18:29:12 +0200

    Add files via upload

---
+++ CourseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ItiDays.Models;
+namespace ItiDays.Controllers
+{
+    public class CourseController : Controller
+    {
+        ITIEntities Data = new ITIEntities();
+        // GET: Course
+        public ActionResult Index()
+        {
+            List<Course> crs = Data.Courses.ToList();
+            return View(crs);
+        }
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Depts = Data.Departments.ToList();
+            return View(Data.Courses.FirstOrDefault(c => c.ID == id));
+        }
+        public ActionResult SaveEdit(int id, Course Newcrs)
+        {
+            Course crs = Data.Courses.FirstOrDefault(c => c.ID == id);
+            crs.Name = Newcrs.Name;
+            crs.Hours = Newcrs.Hours;
+            crs.DeptID = Newcrs.DeptID;
+            Data.SaveChanges();
+            return RedirectToAction("Detials","Department",new { id = crs.DeptID });
+        }
+    }
+}
---
+++ DepartmentController.cs
@@ -0,0 +1,72 @@
+﻿using ItiDays.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItiDays.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ITIEntities Data = new ITIEntities();
+        public ActionResult Search(string DeptName)
+        {
+            ViewBag.DeptName = DeptName;
+            List<Department> depts = Data.Departments.Where(d => d.Name.Contains(DeptName)).ToList();
+            return View("GetAll", depts);
+        }
+        public ActionResult GetAll()
+        {
+            List<Department> depts = Data.Departments.ToList();
+            return View(depts);
+        }
+        public ActionResult Detials(int id)
+        {
+            return View(Data.Departments.SingleOrDefault(d => d.ID == id));
+        }
+        
+        public ActionResult TestVB()
+        {
+            List<string> StudentNames = new List<string>();
+            StudentNames.Add("osman");
+            StudentNames.Add("ali");
+            StudentNames.Add("mohamed");
+            StudentNames.Add("mansour");
+            ViewData["names"] = StudentNames;
+            return View();
+        }
+        public ActionResult NewDept()
+        {
+            return View();
+        }
+        public ActionResult SaveNewDept(Department dept)
+        {
+            
+            
+            #region request object
+            /*
+            string name = "", manager = "";
+            if(Request.QueryString["Name"]!="")
+            {
+                name = Request.QueryString["Name"];
+            }
+            if(Request.QueryString["Manager"] != "")
+            {
+                manager = Request.QueryString["Manager"];
+            }
+            */
+            #endregion
+            
+            if (dept.Name != "" && dept.Manager != "")
+            {
+                
+                
+                Data.Departments.Add(dept);
+                Data.SaveChanges();
+                return RedirectToAction("GetAll");
+            }
+            return View();
+        }
+    }
+}
